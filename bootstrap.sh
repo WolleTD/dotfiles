8d9152198fb873064396021211670d8cc48a45c6
Eicke Herbertz <wolletd@posteo.de>
2025-09-27 01:26:08 +0200

    Update bootstrap.sh to be idempotent

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,37 +6,47 @@ if [[ $(pwd) != $HOME || $(realpath $DOTFILE_DIR) == $DOTFILE_DIR ]]; then
     exit 1
 fi
 
-if [[ -f $HOME/.tmux.conf || -f $HOME/.vimrc || -f $HOME/.zshrc \
-    || -f $HOME/.tmux || -f $HOME/.vim ]]; then
-    echo "Files exist, aborting!"
-    exit 1
-fi
-
 [[ -z $XDG_CONFIG_HOME ]] && XDG_CONFIG_HOME=$HOME/.config
 
 [[ -x $(which tmux) ]] || (echo "Error: tmux not installed!" >&2; exit 1)
 
 echo "Updating submodules..."
-pushd $DOTFILE_DIR
+pushd $DOTFILE_DIR >/dev/null
 git submodule sync
 git submodule update --init
-popd
+popd >/dev/null
 
-echo "Linking .tmux.conf"
-ln -s $DOTFILE_DIR/tmux/tmux.conf .tmux.conf
-echo "Downloading tmux plugins..."
-tmux new-session -d sh -c "$DOTFILE_DIR/tmux/plugins/tpm/bin/install_plugins|tee /tmp/dotfiles.log"
-cat /tmp/dotfiles.log
-echo "Linking .zshrc"
-ln -s $DOTFILE_DIR/zshrc .zshrc
-echo "Linking .vimrc"
-ln -s $DOTFILE_DIR/vim/vimrc .vimrc
-ln -s $DOTFILE_DIR/vim/ideavimrc .ideavimrc
-echo "Linking $XDG_CONFIG_HOME/git/config"
+function symlink() {
+    local target=$1
+    local name=$2
+
+    if [[ -d "$name" ]]; then
+        echo "$name is a directory, not linking!" >&2
+    elif [[ -L "$name" && "$(readlink "$name")" == "$target" ]]; then
+        echo "$name symlink already correct."
+    elif ln -s "$target" "$name"; then
+        echo "Created symlink $name -> $target"
+    fi
+}
+
+symlink $DOTFILE_DIR/zshrc .zshrc
+symlink $DOTFILE_DIR/vim/vimrc .vimrc
+symlink $DOTFILE_DIR/vim/ideavimrc .ideavimrc
+symlink $DOTFILE_DIR/tmux/tmux.conf .tmux.conf
+
 mkdir -p $XDG_CONFIG_HOME/git
-ln -s $(realpath --relative-to=$XDG_CONFIG_HOME/git $DOTFILE_DIR)/gitconfig \
-    $XDG_CONFIG_HOME/git/config
-# Create ~/.gitconfig so git config --global won't write to $XDG...
-touch $HOME/.gitconfig
-echo "Set global git excludesfile in ~/.gitignore"
-git config --global core.excludesfile $(realpath $DOTFILE_DIR)/global-gitignore
+dotfile_dir_rel=$(realpath --relative-to=$XDG_CONFIG_HOME/git $DOTFILE_DIR)
+symlink $dotfile_dir_rel/gitconfig $XDG_CONFIG_HOME/git/config
+
+echo "Downloading tmux plugins..."
+mkfifo /tmp/tmux_fifo
+tmux new-session -d "$DOTFILE_DIR/tmux/plugins/tpm/bin/install_plugins > /tmp/tmux_fifo"
+cat /tmp/tmux_fifo
+rm /tmp/tmux_fifo
+
+if [ ! -e .gitconfig ]; then
+    echo "Create ~/.gitconfig so git config --global won't write to \$XDG_CONFIG_HOME/git/config"
+    echo "# This file is system local. $XDG_CONFIG_HOME/git/config is symlinked to .dotfiles" > .gitconfig
+else
+    echo ".gitconfig already exists."
+fi
